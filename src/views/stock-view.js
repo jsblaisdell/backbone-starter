c9617d21d1b6234d0f13ddaeb3dca293ae1ad1b5
Justin Blaisdell <jsblaisdell@gmail.com>
2013-10-04 17:44:45 -0500

    change color of price when it updates

--- src/views/stock-view.js
+++ src/views/stock-view.js
@@ -4,13 +4,23 @@
     className: 'stock',
     template: _.getTemplate("stock"),
     initialize: function(options){
-      // Listen for its model's changes in initialize.
       this.listenTo(this.model, 'change', function (){
         this.render();
+        var prevPrice = this.model.previous('price');
+        var newPrice  = this.model.get('price');
+        var priceSpan = $(this.el).find('.price');
+        if (prevPrice === undefined) {
+        } else if (prevPrice < newPrice) {
+          priceSpan.removeClass('bad-change');
+          priceSpan.addClass('good-change');
+        } else {
+          priceSpan.removeClass('good-change');
+          priceSpan.addClass('bad-change');
+        }
+        console.log('change', this.model.get('name'), 'to', newPrice, 'from', prevPrice);
       });
     },
     render: function(){
-      // updates its element `el` using an underscore template.
       var stockName = this.model.get('name');
       var stockPrice = this.model.get('price');
       var newHtml = this.template({name: stockName, price: stockPrice});
